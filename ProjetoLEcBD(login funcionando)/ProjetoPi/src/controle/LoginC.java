a854d84046d8090564ea13846646aadf250349c6
Wellingtomn <39540209+Wellingtomn@users.noreply.github.com>
2018-05-28 18:50:04 -0300

    Add files via upload

---
+++ ProjetoLEcBD(login funcionando)/ProjetoPi/src/controle/LoginC.java
@@ -0,0 +1,43 @@
+package controle;
+
+import modelo.dao.LoginDAO;
+
+/**
+ *
+ * @author Jefferson
+ */
+public class LoginC {
+    
+    /*###################################
+              ATRIBUTOS DA CLASSE
+      ###################################*/
+    
+    private final LoginDAO loginDao;
+
+    /*###################################
+              CONSTRUTOR DA CLASSE
+      ###################################*/
+    
+    public LoginC() {
+        
+       this.loginDao = new LoginDAO();
+        
+    }
+    
+    /*###################################
+               MÉTODOS DA CLASSE
+      ###################################*/
+    
+    public boolean autenticarUsuario(String l, String s){
+        
+        if (!"".equals(l) || !"".equals(s)){
+            
+            return this.loginDao.autenticarUsuario(l, s);
+            
+        }
+        
+        return false;
+        
+    }
+    
+}
